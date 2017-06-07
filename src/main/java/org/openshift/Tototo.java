c50b6699cd07eaebaa96a00fc0a9b73295a84de2
David JP Martins <david.martins@lzlabs.com>
2017-06-07 15:23:59 +0200

    New change

---
+++ src/main/java/org/openshift/Tototo.java
@@ -0,0 +1,18 @@
+package org.openshift;
+
+import java.util.HashMap;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@RequestScoped
+@Path("/tototo")
+public class Tototo {
+	
+	@GET()
+	@Produces("application/json")
+	public String getInsult() {
+		return "toto";
+	}
+}
